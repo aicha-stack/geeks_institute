6d39a33bf2aff18dc0da32fb1d9ae7aa3d755234
aicha-stack <aicha12ezzahraoui@gmail.com>
2025-08-19 12:53:19 +0100

    send again

--- week2/day1/daily challenge/challenge.sql
+++ week2/day1/daily challenge/challenge.sql
@@ -4,4 +4,4 @@ SELECT COUNT(*) FROM actors;
 INSERT INTO actors (first_name, last_name, age, number_oscars)
 VALUES ('Tom', '', NULL, 0);
 
-
+--Une erreur se produit parce que certaines colonnes de ta table sont définies comme NOT NULL
